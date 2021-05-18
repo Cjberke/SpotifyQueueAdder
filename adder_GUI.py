4e175ae291f0fc73ed4fd2acaab6ad384506f29c
Cjberke <cjberke@gmail.com>
2021-05-17 23:58:10 -0500

    Testing branching lol

--- adder_GUI.py
+++ adder_GUI.py
@@ -5,18 +5,13 @@ class GUI:
     
     def __init__(self):
         self.root = Tk()
-        self.make_window()
-        self.debug_button()
-        self.start_button()
-        self.stop_button()
-        self.root.mainloop()
 
-    def make_window(self):
+        #Make the App window
         self.root.geometry('720x480')
         self.root.iconbitmap('resources/maple_leaf.ico')
         self.root.title('SpotifyQueueAdder')
     
-    def debug_button(self):
+        #Make Debug button
         self.debug_frame = LabelFrame(self.root, padx=15,pady=15)
         self.debug_frame.grid(column=0,row=0)
         self.debug_but = Button(self.debug_frame, 
@@ -24,18 +19,17 @@ class GUI:
                                    command=self.debug_time)     
         self.debug_but.pack()  
     
-    def start_button(self):
+        #Make Start Button
         self.start_frame = LabelFrame(self.root, padx=25,pady=25)
         self.start_frame.grid(column=1,row=0)
         self.start_but = Button(self.start_frame, 
                                    text='Click Here to Start Building', 
                                    command=self.test_func)
-        
         self.start_but.pack()
         self.start_label = Label(self.root, text='')
         self.start_label.grid(column=1,row=1)
     
-    def stop_button(self):
+        #Make Exit Button
         self.exit_frame = LabelFrame(self.root, padx=10,pady=10)
         self.exit_frame.grid(column=0,row=2)
         
@@ -43,6 +37,7 @@ class GUI:
                                   text='Click Here to Exit', 
                                   command=self.root.quit)
         self.exit_button.pack()
+        self.root.mainloop()
     
     def test_func(self):
         self.build_label = Label(self.root, text="Building...")
